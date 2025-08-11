4ea419db7a99fe0bd897e9e0f63bc4ed755a8f5f
Danil <vahidov@gmail.com>
2025-08-11 21:53:36 +0200

    Item transfers (#8)
    * transfer logs
    
    * Auto-generated SDK
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- gen/grpc/js/inventory/inventory_grpc_web_pb.js
+++ gen/grpc/js/inventory/inventory_grpc_web_pb.js
@@ -862,5 +862,66 @@ proto.inventory.v1.InventoryServicePromiseClient.prototype.listItemsByItemDef =
 };
 
 
+/**
+ * @const
+ * @type {!grpc.web.MethodDescriptor<
+ *   !proto.inventory.v1.TransferLogRequest,
+ *   !proto.inventory.v1.TransferLogResponse>}
+ */
+const methodDescriptor_InventoryService_TransferLog = new grpc.web.MethodDescriptor(
+  '/inventory.v1.InventoryService/TransferLog',
+  grpc.web.MethodType.UNARY,
+  proto.inventory.v1.TransferLogRequest,
+  proto.inventory.v1.TransferLogResponse,
+  /**
+   * @param {!proto.inventory.v1.TransferLogRequest} request
+   * @return {!Uint8Array}
+   */
+  function(request) {
+    return request.serializeBinary();
+  },
+  proto.inventory.v1.TransferLogResponse.deserializeBinary
+);
+
+
+/**
+ * @param {!proto.inventory.v1.TransferLogRequest} request The
+ *     request proto
+ * @param {?Object<string, string>} metadata User defined
+ *     call metadata
+ * @param {function(?grpc.web.RpcError, ?proto.inventory.v1.TransferLogResponse)}
+ *     callback The callback function(error, response)
+ * @return {!grpc.web.ClientReadableStream<!proto.inventory.v1.TransferLogResponse>|undefined}
+ *     The XHR Node Readable Stream
+ */
+proto.inventory.v1.InventoryServiceClient.prototype.transferLog =
+    function(request, metadata, callback) {
+  return this.client_.rpcCall(this.hostname_ +
+      '/inventory.v1.InventoryService/TransferLog',
+      request,
+      metadata || {},
+      methodDescriptor_InventoryService_TransferLog,
+      callback);
+};
+
+
+/**
+ * @param {!proto.inventory.v1.TransferLogRequest} request The
+ *     request proto
+ * @param {?Object<string, string>=} metadata User defined
+ *     call metadata
+ * @return {!Promise<!proto.inventory.v1.TransferLogResponse>}
+ *     Promise that resolves to the response
+ */
+proto.inventory.v1.InventoryServicePromiseClient.prototype.transferLog =
+    function(request, metadata) {
+  return this.client_.unaryCall(this.hostname_ +
+      '/inventory.v1.InventoryService/TransferLog',
+      request,
+      metadata || {},
+      methodDescriptor_InventoryService_TransferLog);
+};
+
+
 module.exports = proto.inventory.v1;
 
